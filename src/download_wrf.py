0f7ebb945e845bf4681aeec3df7344542d246b34
shafeerkb <shafeerkb@gmail.com>
2020-04-07 11:04:53 +0530

    download_module

---
+++ src/download_wrf.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 22:19:41 2020
+
+@author: vassar
+"""
+
+import pandas as pd
+
+
+import time
+start_time = time.time()
+print(time.time() - start_time)
+
+import os
+
+date_time=pd.date_range(start='2020-04-06 00:00:00', periods=75,freq='H')
+
+for tm in date_time[1:]:
+    nc_url=date_time[0].strftime("%Y%m%d%H/")+"WRF3km_INDIA-"+tm.strftime("%Y-%m-%d_%H.nc")
+    YRL="axel -a -n 4 ftp://nwp:nwp@125.21.185.50/nwp-data/WRF_NETCDF/"+nc_url
+    print(YRL)
+    os.system(YRL)
+    
+    
